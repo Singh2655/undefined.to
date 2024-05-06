56ce7db062b4cff769c483abcb83cee2c1a20777
Singh2655 <singhshaurya222002@gmail.com>
2024-05-06 14:40:06 +0530

    send-answer typefix

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const messageSchema = z.object({
   content: z.string().min(1, { message: "Message must be of 1 character." }).max(200, { message: "Message must be no more than 200 characters long." }),
   answer:z.string().max(200, { message: "Answer must be no more than 200 characters long." }).optional(),
-  isAnswered:z.boolean().default(false)
+  isAnswered:z.boolean().default(false).optional()
 });
